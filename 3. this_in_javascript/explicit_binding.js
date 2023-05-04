5c62953aa8352433814bafb7a23471a69573b56b
Suneeth <suneeth.since2005@gmail.com>
2023-05-04 13:12:28 +0530

    :new:  Update explicit binding

--- 3. this_in_javascript/explicit_binding.js
+++ 3. this_in_javascript/explicit_binding.js
@@ -5,16 +5,26 @@
  * method then this keyword default reference is changed to the object called using the above-specified methods.
 */
 
-function verifyAge() {
+function verifyAge(car) {
     let age = this.age;
 
     if (age > 18) {
-        console.log("You can drive");
+        console.log(`You can drive ${car}`);
     } else {
-        console.log("You can't drive");
+        console.log(`You can't drive ${car}`);
     }
 }
 
 const person = { age: 22 };
-
-verifyAge.call(person);
+const car = "Supra";
+// call method
+// in this method we need to pass arguments one by one
+verifyAge.call(person, car);
+// apply method
+// in this method we can pass any type of arguments
+verifyAge.apply(person, car);
+// bind method
+// bind method returns a new funtion that need to invoke
+// in this method we need to pass arguments one by one
+const newVerifyFunc = verifyAge.bind(person, car);
+newVerifyFunc();
