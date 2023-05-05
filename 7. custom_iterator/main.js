8effb9a6f71c703495a0fa37b01a55dbbbc150ff
Suneeth <suneeth.since2005@gmail.com>
2023-05-05 18:15:42 +0530

    :tada: Add custom iterators

---
+++ 7. custom_iterator/main.js
@@ -0,0 +1,27 @@
+/**
+ * Custom Iterator
+ * ---------------
+ * */
+
+// this message object a custom made iterator
+const messages = {
+	[Symbol.iterator]: function () {
+		let step = 0;
+		const iterator = {
+			next: function () {
+				step++;
+				if (step === 1) {
+					return { value: "Hey", done: false };
+				} else if (step === 2) {
+					return { value: "Dude", done: false };
+				}
+				return { value: undefined, done: true };
+			}
+		}
+		return iterator;
+	}
+}
+
+for (const msg of messages) {
+	console.log(msg);
+}
